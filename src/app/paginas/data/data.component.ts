c4fbd1ab504f95fac6196bc9243fa7e702f6af2a
Quinten <90308786+QNoah@users.noreply.github.com>
2024-06-03 08:21:31 +0200

    alle functies werken nu op data detail pagina

--- src/app/paginas/data/data.component.ts
+++ src/app/paginas/data/data.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Data } from 'src/app/modelen/data.model';
 import { DataService } from 'src/app/services/data/data.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router  } from '@angular/router';
 import { catchError, of } from 'rxjs';
 
 @Component({
@@ -10,12 +10,14 @@ import { catchError, of } from 'rxjs';
   styleUrls: ['./data.component.scss'],
 })
 export class DataComponent implements OnInit {
-  data: any = null;
-  id: number;
+  data: any;
+  id: number = this.route.snapshot.params['id'];
+  dataBestaatNiet: boolean;
 
   constructor(
     private dataService: DataService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
@@ -23,12 +25,12 @@ export class DataComponent implements OnInit {
   }
 
   haalBestaandeDataOp(): void {
-    this.id = this.route.snapshot.params['id'];
     this.dataService
       .laadDataOpId(this.id)
       .pipe(
         catchError((err) => {
           console.log(err);
+          this.dataBestaatNiet = true;
           this.data = { id: this.id };
           return of(null); // Gebruik 'of(null)' om een lege observable te retourneren in geval van een fout
         })
@@ -36,7 +38,48 @@ export class DataComponent implements OnInit {
       .subscribe((data: any) => {
         if (data !== null) {
           this.data = data;
+          this.dataBestaatNiet = false;
+        } else {
+          this.dataBestaatNiet = true;
         }
       });
   }
+
+  annuleren(){
+    const bevestigd = confirm('Weet je zeker dat je mogelijke wijzigingen wilt negeren en terug wilt gaan naar het dashboard?');
+    if(bevestigd){
+      this.router.navigate(['Dashboard']);
+    }
+  }
+  verwijderen(){
+    const bevestigd = confirm('Weet je zeker dat je deze data wilt verwijderen?');
+    if(bevestigd){
+      this.dataService.verwijderOpId(this.id).subscribe(() => {
+        this.data = this.data.filter((data) => data.id!== this.id);
+      },
+      (error) => {
+        console.log('Error', error);
+      }
+    );
+    }
+  }
+
+opslaan(){
+  const bevestigd = confirm('Weet je zeker dat je de wijzigingen wilt opslaan?');
+  if (bevestigd){
+  if (this.dataBestaatNiet === true){
+    this.dataService.postData(this.data).subscribe((data) => {
+      this.data = data;
+      console.log(this.data)
+      this.router.navigate(['Dashboard']);
+    })
+  } else {
+    this.dataService.updateData(this.data.id, this.data).subscribe((response: any) => {
+      console.log(response);
+      console.log('Bestaat al')
+      this.router.navigate(['Dashboard']);
+    });
+  }
+}
+}
 }
--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -1,24 +1,57 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
 import { Data } from 'src/app/modelen/data.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class DataService{
+export class DataService {
   baseUrl = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) { }
-  
-  laadData():Observable<Data>{
+  constructor(private http: HttpClient) {}
+
+  laadData(): Observable<Data> {
     return this.http.get<Data>(`${this.baseUrl}/data`);
   }
-  verwijderOpId(id: number): Observable<any>{
+  verwijderOpId(id: number): Observable<any> {
     return this.http.delete(`${this.baseUrl}/data/${id}`);
   }
 
-  laadDataOpId(id: number){
+  laadDataOpId(id: number) {
     return this.http.get<Data>(`${this.baseUrl}/data/${id}`);
+  }
+  postData(data: Data): Observable<Data> {
+    const body = JSON.stringify(data);
+    console.log(body);
+    return this.http
+      .post<Data>(`${this.baseUrl}/data`, body, {
+        headers: this.getHeaders(),
+      })
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          return of(new Data());
+        })
+      );
+  }
+  updateData(Id: number, data: Data): Observable<Data> {
+    const body = JSON.stringify(data);
+    return this.http
+      .put<Data>(`${this.baseUrl}/data/${Id}`, body, {
+        headers: this.getHeaders(),
+      })
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          return of(new Data());
+        })
+      );
+  }
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders()
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json');
   }
 }
